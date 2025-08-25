96a4b551a0f03f318fb6619ccda7d0a23afd5e6b
Sree Raghuveer Pedapudi <sreeraghuveerpedapudi@gmail.com>
2025-08-25 12:51:25 -0500

    Add files via upload

---
+++ SREE_MATLAB/Homework_1.m
@@ -0,0 +1,15 @@
+clc
+clear
+close all
+
+%Homework 1-Sree Raghuveer Pedapudi
+%%Problem 1
+x1 = rand(10);
+y1 = rand(10);
+y_model1=LinearLeastSquare(x1,y1);
+%Problem 2
+
+function y_model1=LinearLeastSquare(x,y)
+
+
+end
